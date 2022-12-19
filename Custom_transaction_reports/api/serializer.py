d13d45edec61049cba8403c3d9c246a38df5557a
Pardhu Inti <pardhuganesh21@gmail.com>
2022-12-19 16:26:44 +0530

    Added ddress to serilaizer

--- Custom_transaction_reports/api/serializer.py
+++ Custom_transaction_reports/api/serializer.py
@@ -4,13 +4,17 @@ from .models import Transaction
 class TransactionSerializer(serializers.ModelSerializer):
     total_amount = serializers.SerializerMethodField()
     product_id_quantity = serializers.SerializerMethodField()
+    address =serializers.SerializerMethodField()
 
     class Meta:
         model = Transaction
-        fields = ['id', 'customer', 'merchant', 'product_id_quantity', 'total_amount', 'timestamp']
+        fields = ['id', 'customer', 'merchant', 'product_id_quantity', 'total_amount', 'address', 'timestamp']
 
     def get_total_amount(self, obj):
         return obj.total_amount()
 
     def get_product_id_quantity(self, obj):
         return obj.quantity_list()
+
+    def get_address(self,obj):
+        return obj.customer.address
