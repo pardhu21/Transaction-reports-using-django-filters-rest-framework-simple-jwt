f57532fc3db786934f32612be0d3fb68710bd7fb
Pardhu Inti <pardhuganesh21@gmail.com>
2022-12-23 23:25:43 +0530

    Added Tokens class to store token

--- Custom_transaction_reports/auditor/urls.py
+++ Custom_transaction_reports/auditor/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('register', register, name='register'),
     path('login-user', login_user, name='login-user'),
     path('register-user', register_user, name='register-user'),
+    path('logout', logout_user, name='logout'),
     path('dashboard', dashboard, name='dashboard')
 ]
--- Custom_transaction_reports/auditor/views.py
+++ Custom_transaction_reports/auditor/views.py
@@ -5,7 +5,10 @@ import requests
 from django.http import HttpResponse
 
 # Create your views here.
-token = ''
+class Tokens:
+    token = ''
+    refresh = ''
+
 def home(request):
     return render(request, 'auditor/home.html')
 
@@ -14,6 +17,10 @@ def login_page(request):
 
 def register(request):
     return render(request, 'auditor/register.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
 
 def login_user(request):
     if request.method == 'POST':
@@ -28,9 +35,8 @@ def login_user(request):
         if data.status_code == 200:
             data = data.json()
             user = User.objects.get(username = username)
-            global token
-            token = data['token']['access']
-            print(token)
+            Tokens.token = data['token']['access']
+            Tokens.refresh = data['token']['refresh']
             login(request, user)
             return redirect('dashboard')
         
@@ -49,14 +55,23 @@ def register_user(request):
         if data.status_code == 201:
             data = data.json()
             user = User.objects.get(username = username)
-            global token
-            token = data['token']['access']
-            print(token)
+            Tokens.token = data['token']['access']
+            Tokens.refresh = data['token']['refresh']
             login(request, user)
             return redirect('dashboard')
 
 def dashboard(request):
     url = 'http://127.0.0.1:8000/api/transaction'
-    data = requests.get(url = url, headers={'authorization':f'Bearer {token}', 'content-type': 'application/json'})
-    print('Bearer ' + token)
-    return HttpResponse(data.json())
+    data = requests.get(url = url, headers={'authorization':f'Bearer {Tokens.token}', 'content-type': 'application/json'})
+    if data.status_code == 200:
+        return render(request, 'auditor/base-dashboard.html', {'data' : data.json()})
+    if data.status_code == 401:
+       get_new_token()
+       return redirect('dashboard')
+
+def get_new_token():
+    url = 'http://127.0.0.1:8000/api/token/refresh/'
+    data = requests.post(url = url, data = {"refresh": Tokens.refresh}).json()  
+    Tokens.token = ['access']
+    
+                                        
